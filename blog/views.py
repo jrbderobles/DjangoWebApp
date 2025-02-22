2e0e3ce355ffce1754d425dc89109b4d58ac2d13
John de Robles <jrbderobles@gmail.com>
2025-02-23 00:20:58 +0800

    [Blog] Created blog templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author':      'John Doe',
+        'title':       'Blog Post 1',
+        'content':     'First Post Content',
+        'date_posted': 'August 1, 2024'
+    },
+    {
+        'author':      'Jane Doe',
+        'title':       'Blog Post 2',
+        'content':     'Second Post Content',
+        'date_posted': 'August 3, 2024'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
